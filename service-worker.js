56a042f6b56773f70fc7fd0654e0f55a9e9fbbd2
Filip Musiał <fimusial@gmail.com>
2023-09-06 01:13:54 +0200

    fixed sending response via chrome.runtime

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,18 @@
 // https://developer.chrome.com/docs/extensions/mv3/messaging/#simple
-chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-    const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-
-    // https://developer.chrome.com/docs/extensions/mv3/content_scripts/#programmatic
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.triggerBlade && typeof (request.triggerBlade) === 'string') {
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            files: [`blades/${request.triggerBlade}.js`]
-        });
-    }
+        (async () => {
+            const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
 
-    sendResponse('request processed');
+            // https://developer.chrome.com/docs/extensions/mv3/content_scripts/#programmatic
+            chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                files: [`blades/${request.triggerBlade}.js`]
+            });
+
+            sendResponse(`blade triggered: ${request.triggerBlade}`);
+        })();
+
+        return true;
+    }
 });
