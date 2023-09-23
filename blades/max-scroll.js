d21f54659bdaed1890b420826ca9761add787706
Filip Musiał <fimusial@gmail.com>
2023-09-23 16:51:20 +0200

    simplified variable names, fixed a bug where max-scroll would stop working after scrolling up

--- blades/max-scroll.js
+++ blades/max-scroll.js
@@ -1,16 +1,18 @@
 export function blade_maxScrollStart() {
-	if (window.chromePwr_maxScrollLastTimeoutId > 0) {
+	if (window.lastTimeoutId > 0) {
 		return;
 	}
 
-	window.chromePwr_maxScrollPrevY = 0;
-	window.chromePwr_maxScrollLastTimeoutId = 0;
+	window.prevY = 0;
+	window.lastTimeoutId = 0;
 
 	const next = () => {
-		if (window.chromePwr_maxScrollPrevY <= window.scrollY) {
-			window.chromePwr_maxScrollPrevY = window.scrollY;
+		if (window.prevY <= window.scrollY) {
+			window.prevY = window.scrollY;
 			window.scrollTo(0, Number.MAX_SAFE_INTEGER);
-			window.chromePwr_maxScrollLastTimeoutId = setTimeout(next, 0);
+			window.lastTimeoutId = setTimeout(next, 0);
+		} else {
+			window.lastTimeoutId = 0;
 		};
 	}
 
@@ -18,6 +20,6 @@ export function blade_maxScrollStart() {
 }
 
 export function blade_maxScrollCancel() {
-	clearTimeout(window.chromePwr_maxScrollLastTimeoutId);
-	window.chromePwr_maxScrollLastTimeoutId = 0;
+	clearTimeout(window.lastTimeoutId);
+	window.lastTimeoutId = 0;
 }
