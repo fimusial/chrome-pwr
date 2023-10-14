390f299ba9b297a794e1bc8815d99e810377cfb3
Filip Musiał <fimusial@gmail.com>
2023-10-14 14:45:29 +0200

    edit page content feature

--- chrome-pwr.js
+++ chrome-pwr.js
@@ -21,3 +21,15 @@ document.getElementById('forced-css-clear').onclick = async () => {
         triggerBlade: 'forcedCssClear'
     });
 };
+
+document.getElementById('content-edit-on').onclick = async () => {
+    await chrome.runtime.sendMessage({
+        triggerBlade: 'contentEditOn'
+    });
+};
+
+document.getElementById('content-edit-off').onclick = async () => {
+    await chrome.runtime.sendMessage({
+        triggerBlade: 'contentEditOff'
+    });
+};
