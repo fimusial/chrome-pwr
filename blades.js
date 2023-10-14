46a709074b1bea3293eb2179ebc79a90d465c8e3
Filip Musiał <fimusial@gmail.com>
2023-10-14 14:45:29 +0200

    edit page content feature

--- blades.js
+++ blades.js
@@ -1,2 +1,3 @@
 export * from './blades/max-scroll.js';
 export * from './blades/forced-css.js';
+export * from './blades/content-edit.js';
---
+++ blades/content-edit.js
@@ -0,0 +1,7 @@
+export function blade_contentEditOn() {
+    document.body.contentEditable = 'true';
+}
+
+export function blade_contentEditOff() {
+    document.body.contentEditable = 'false';
+}
