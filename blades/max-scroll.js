ab278e7899c1174ddd82ec1349a0910742f23be7
Filip Musiał <fimusial@gmail.com>
2023-12-04 20:53:36 +0100

    format max-scroll.js (tabs -> spaces)

--- blades/max-scroll.js
+++ blades/max-scroll.js
@@ -1,25 +1,25 @@
 export function blade_maxScrollStart() {
-	if (window.lastTimeoutId > 0) {
-		return;
-	}
+    if (window.lastTimeoutId > 0) {
+        return;
+    }
 
-	window.prevY = 0;
-	window.lastTimeoutId = 0;
+    window.prevY = 0;
+    window.lastTimeoutId = 0;
 
-	const next = () => {
-		if (window.prevY <= window.scrollY) {
-			window.prevY = window.scrollY;
-			window.scrollTo(0, Number.MAX_SAFE_INTEGER);
-			window.lastTimeoutId = setTimeout(next, 100);
-		} else {
-			window.lastTimeoutId = 0;
-		};
-	}
+    const next = () => {
+        if (window.prevY <= window.scrollY) {
+            window.prevY = window.scrollY;
+            window.scrollTo(0, Number.MAX_SAFE_INTEGER);
+            window.lastTimeoutId = setTimeout(next, 100);
+        } else {
+            window.lastTimeoutId = 0;
+        };
+    }
 
-	next();
+    next();
 }
 
 export function blade_maxScrollCancel() {
-	clearTimeout(window.lastTimeoutId);
-	window.lastTimeoutId = 0;
+    clearTimeout(window.lastTimeoutId);
+    window.lastTimeoutId = 0;
 }
