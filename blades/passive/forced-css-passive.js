3072366b7383f89a60619c5fb01f9323d1be86bd
Filip Musiał <fimusial@gmail.com>
2024-02-17 01:26:13 +0100

    added recording and playback indicators

--- blades/passive/forced-css-passive.js
+++ blades/passive/forced-css-passive.js
@@ -1,3 +1,6 @@
-const style = document.createElement('style');
-style.innerHTML = localStorage.getItem('chrome-pwr-forced-css');
-document.head.appendChild(style);
+const forcedCss = localStorage.getItem('chrome-pwr-forced-css');
+if (forcedCss) {
+    const style = document.createElement('style');
+    style.innerHTML = forcedCss;
+    document.head.appendChild(style);
+}
