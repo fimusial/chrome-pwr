d3a44a0df6503684c6a1ef87e58ab77aab194850
Filip Musiał <fimusial@gmail.com>
2024-05-26 20:42:03 +0200

    refactoring, implemented an audio tap

--- spectro/spectro.js
+++ spectro/spectro.js
@@ -70,7 +70,7 @@ class SpectrogramGrid {
 };
 
 const randomVector = (count, minValue, maxValue) => {
-    let vector = [];
+    const vector = [];
     for (let i = 0; i < count; i++) {
         vector.push(minValue + Math.floor(Math.random() * maxValue));
     }
@@ -84,12 +84,15 @@ const hRes = 30;
 const maxValue = 100;
 const spectrogram = new SpectrogramGrid(wRes, hRes, maxValue);
 
+// todo: placeholder with sine waves
 const spectrogramNoiseInterval = setInterval(() => {
     spectrogram.push(randomVector(wRes, 0, maxValue));
     spectrogram.draw(canvas);
 }, 25);
 
 document.getElementById('spectro-tab-button').onclick = async (command) => {
+    // https://github.com/GoogleChrome/chrome-extensions-samples/tree/main/functional-samples/sample.tabcapture-recorder
+
     const existingContexts = await chrome.runtime.getContexts({});
     const offscreenDocument = existingContexts.find((context) => context.contextType === 'OFFSCREEN_DOCUMENT');
 
@@ -97,7 +100,7 @@ document.getElementById('spectro-tab-button').onclick = async (command) => {
 
     if (!offscreenDocument) {
         await chrome.offscreen.createDocument({
-            url: 'spectro/offscreen-audio.html',
+            url: 'spectro/offscreen-audio-hub.html',
             reasons: ['USER_MEDIA'],
             justification: 'Chrome PWR tab audio capture'
         });
@@ -108,13 +111,13 @@ document.getElementById('spectro-tab-button').onclick = async (command) => {
     const tabNameSpan = document.getElementById('spectro-tab-name-span');
 
     if (capturing) {
-        chrome.runtime.sendMessage({ spectro: 'stopCapture' });
+        chrome.runtime.sendMessage({ audioHub: 'stopCapture' });
         tabNameSpan.innerText = '';
         return;
     }
 
     const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
     const streamId = await chrome.tabCapture.getMediaStreamId({ targetTabId: tab.id });
-    chrome.runtime.sendMessage({ spectro: 'startCapture', params: { streamId } });
+    chrome.runtime.sendMessage({ audioHub: 'startCapture', params: { streamId } });
     tabNameSpan.innerText = tab.title;
 };
