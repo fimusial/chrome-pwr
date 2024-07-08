b6967f0bfa6723b298b90aad4b11aa778800b407
Filip Musiał <fimusial@gmail.com>
2024-07-08 22:57:35 +0200

    create the offscreen document only once on chrome startup; refactor; simplify

--- spectro/spectro.js
+++ spectro/spectro.js
@@ -35,58 +35,6 @@ const nextVisualizerDraw = () => {
 
 nextVisualizerDraw();
 
-const tabButton = document.getElementById('spectro-capture-button');
-const tabNameSpan = document.getElementById('spectro-tab-name-span');
-const recordingButton = document.getElementById('spectro-recording-button');
-let capturedTabId = 0;
-
-const setCaptureInfo = (tab) => {
-    tabNameSpan.innerText = tab.title;
-    capturedTabId = tab.id;
-    tabButton.setAttribute('audioCaptureLive', 'true');
-    recordingButton.setAttribute('audioCaptureLive', 'true');
-};
-
-const clearCaptureInfo = () => {
-    tabNameSpan.innerText = '';
-    capturedTabId = 0;
-    tabButton.removeAttribute('audioCaptureLive');
-    recordingButton.removeAttribute('audioCaptureLive');
-};
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (tabId === capturedTabId && changeInfo.title) {
-        setCaptureInfo(tab);
-    }
-});
-
-const toggleCaptureInfo = () => {
-    if (capturedTabId !== 0) {
-        clearCaptureInfo();
-        return;
-    }
-
-    chrome.runtime.sendMessage({ audioHub: 'getCapturedTabId', params: {} }).then((response) => {
-        if (!response || !response.capturedTabId) {
-            // no tab id and no capture - all good
-            return;
-        }
-
-        chrome.tabs.get(response.capturedTabId, (tab) => {
-            if (tab) {
-                // capture already started
-                setCaptureInfo(tab);
-            } else {
-                // tab was closed while capturing
-                chrome.offscreen.closeDocument();
-                clearCaptureInfo();
-            }
-        });
-    });
-};
-
-toggleCaptureInfo();
-
 const hpSlider = document.getElementById('spectro-hp-slider');
 const lpSlider = document.getElementById('spectro-lp-slider');
 const hpSliderValueDisplay = document.getElementById('spectro-hp-value-display');
@@ -154,35 +102,76 @@ const onFilterSliderInput = async (event) => {
 hpSlider.oninput = onFilterSliderInput;
 lpSlider.oninput = onFilterSliderInput;
 
-tabButton.onclick = async () => {
-    const existingContexts = await chrome.runtime.getContexts({});
-    const offscreenDocumentExists = !!existingContexts.find((context) => context.contextType === 'OFFSCREEN_DOCUMENT');
-    if (offscreenDocumentExists) {
-        chrome.offscreen.closeDocument();
-        toggleCaptureInfo();
+const tabButton = document.getElementById('spectro-capture-button');
+const tabNameSpan = document.getElementById('spectro-tab-name-span');
+const recordingButton = document.getElementById('spectro-recording-button');
+let capturedTabId = 0;
+
+const setCaptureInfo = (tab) => {
+    tabNameSpan.innerText = tab.title;
+    capturedTabId = tab.id;
+    tabButton.setAttribute('audioCaptureLive', 'true');
+    recordingButton.setAttribute('audioCaptureLive', 'true');
+};
+
+const clearCaptureInfo = () => {
+    tabNameSpan.innerText = '';
+    capturedTabId = 0;
+    tabButton.removeAttribute('audioCaptureLive');
+    recordingButton.removeAttribute('audioCaptureLive');
+};
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (tabId === capturedTabId && changeInfo.title) {
+        setCaptureInfo(tab);
+    }
+});
+
+chrome.runtime.sendMessage({ audioHub: 'getCapturedTabId', params: {} }).then((response) => {
+    if (!response || !response.capturedTabId) {
         return;
     }
 
-    await chrome.offscreen.createDocument({
-        url: 'spectro/offscreen-audio-hub.html',
-        reasons: ['USER_MEDIA'],
-        justification: 'Chrome PWR tab audio capture'
-    });
+    chrome.tabs.get(response.capturedTabId, (tab) => {
+        if (tab) {
+            // capture already started
+            setCaptureInfo(tab);
+        } else {
+            // tab was closed while capturing
+            chrome.runtime.sendMessage({
+                audioHub: 'stopTabCapture',
+                params: {}
+            });
 
-    const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-    const streamId = await chrome.tabCapture.getMediaStreamId({ targetTabId: tab.id });
-
-    await chrome.runtime.sendMessage({
-        audioHub: 'startTabCapture',
-        params: {
-            streamId: streamId,
-            tabId: tab.id,
-            hp: filterSliderValueToFrequency(hpSlider.value),
-            lp: filterSliderValueToFrequency(lpSlider.value)
+            clearCaptureInfo();
         }
     });
+});
 
-    toggleCaptureInfo();
+tabButton.onclick = async () => {
+    const response = await chrome.runtime.sendMessage({ audioHub: 'getCapturedTabId', params: {} });
+    if (response && response.capturedTabId > 0) {
+        await chrome.runtime.sendMessage({
+            audioHub: 'stopTabCapture',
+            params: {}
+        });
+
+        clearCaptureInfo();
+    } else {
+        const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+        const streamId = await chrome.tabCapture.getMediaStreamId({ targetTabId: tab.id });
+        await chrome.runtime.sendMessage({
+            audioHub: 'startTabCapture',
+            params: {
+                streamId: streamId,
+                tabId: tab.id,
+                hp: filterSliderValueToFrequency(hpSlider.value),
+                lp: filterSliderValueToFrequency(lpSlider.value)
+            }
+        });
+
+        setCaptureInfo(tab);
+    }
 };
 
 const setRecordingButtonText = (audioRecordingState) => {
