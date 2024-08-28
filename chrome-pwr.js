c5329f2ab680f92d3a8f01b7b72720f89bbf0bfb
Filip Musiał <fimusial@gmail.com>
2024-08-29 01:26:37 +0200

    added foldable sections

--- chrome-pwr.js
+++ chrome-pwr.js
@@ -1,3 +1,48 @@
+window.onload = () => {
+    const maxScrollDirection = localStorage.getItem('max-scroll-direction');
+    if (maxScrollDirection) {
+        document.getElementById(`max-scroll-direction-${maxScrollDirection}`).checked = true;
+    } else {
+        document.getElementById('max-scroll-direction-down').checked = true;
+    }
+
+    const delaySliderValue = localStorage.getItem('macro-recorder-delay-slider');
+    if (delaySliderValue) {
+        document.getElementById('macro-recorder-delay-slider').value = delaySliderValue;
+        document.getElementById('macro-recorder-delay-value').innerText = `${delaySliderValue / 1000}`;
+    }
+
+    const savedShowMisc = localStorage.getItem('show-misc');
+    const showMisc = savedShowMisc === 'true' || savedShowMisc === null;
+    document.getElementById('foldable-misc').classList.toggle('hidden', !showMisc);
+    document.getElementById('foldable-misc-checkbox').checked = showMisc;
+
+    const savedShowMacros = localStorage.getItem('show-macros');
+    const showMacros = savedShowMacros === 'true' || savedShowMacros === null;
+    document.getElementById('foldable-macros').classList.toggle('hidden', !showMacros);
+    document.getElementById('foldable-macros-checkbox').checked = showMacros;
+
+    const savedShowAudio = localStorage.getItem('show-audio');
+    const showAudio = savedShowAudio === 'true' || savedShowAudio === null;
+    document.getElementById('foldable-audio').classList.toggle('hidden', !showAudio);
+    document.getElementById('foldable-audio-checkbox').checked = showAudio;
+};
+
+document.getElementById('foldable-misc-checkbox').onclick = (event) => {
+    document.getElementById('foldable-misc').classList.toggle('hidden', !event.target.checked);
+    localStorage.setItem('show-misc', event.target.checked);
+};
+
+document.getElementById('foldable-macros-checkbox').onclick = (event) => {
+    document.getElementById('foldable-macros').classList.toggle('hidden', !event.target.checked);
+    localStorage.setItem('show-macros', event.target.checked);
+};
+
+document.getElementById('foldable-audio-checkbox').onclick = (event) => {
+    document.getElementById('foldable-audio').classList.toggle('hidden', !event.target.checked);
+    localStorage.setItem('show-audio', event.target.checked);
+};
+
 document.getElementById('max-scroll-start-element').onclick = async () => {
     await chrome.runtime.sendMessage({
         triggerBlade: 'maxScrollStartElement',
@@ -91,22 +136,7 @@ document.querySelectorAll('[name=max-scroll-direction]').forEach((element) => {
 document.getElementById('macro-recorder-delay-slider').oninput = (event) => {
     document.getElementById('macro-recorder-delay-value').innerText = `${event.target.value / 1000}`;
     localStorage.setItem('macro-recorder-delay-slider', event.target.value);
-}
-
-window.onload = () => {
-    const maxScrollDirection = localStorage.getItem('max-scroll-direction');
-    if (maxScrollDirection) {
-        document.getElementById(`max-scroll-direction-${maxScrollDirection}`).checked = true;
-    } else {
-        document.getElementById('max-scroll-direction-down').checked = true;
-    }
-
-    const delaySliderValue = localStorage.getItem('macro-recorder-delay-slider');
-    if (delaySliderValue) {
-        document.getElementById('macro-recorder-delay-slider').value = delaySliderValue;
-        document.getElementById('macro-recorder-delay-value').innerText = `${delaySliderValue / 1000}`;
-    }
-}
+};
 
 chrome.commands.onCommand.addListener(async (command) => {
     const supportedCommands = [
